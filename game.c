6449944e436021b1f67747812577136b15d61999
gabrielcoout <gabriel@gabri>
2024-05-08 14:37:03 -0300

    Alterada a ordme de inicio do jogo e adicionada uma funcao listarJogadas

--- game.c
+++ game.c
@@ -1,147 +1,254 @@
 #include <stdio.h>
 
-//Declaracao das funcoes
+// Declaração das funções
 void imprimirJogo(int campos[8][8]);
 void iniciarJogo(int campos[8][8]);
-int jogada(int jogador, int campos[8][8]);
-int validarJogada(int jogador, int campos[8][8]);
-int jogadasValidas(int campos[8][8]);
+void jogada(int *jogador, int campos[8][8]);
+int validarJogada(int jogador, int linha, int coluna, int campos[8][8]);
+int jogadasValidas(int jogador, int campos[8][8]);
+void listarJogadas(int campos[8][8]);
 void limparJogadasValidas(int campos[8][8]);
+void gameLoop();
+int fimDeJogo(int campos[8][8]);
 
-//funcoes de debug
 void debugJogo(int campos[8][8]);
 
+// Função principal
 int main() {
+    gameLoop();
+    return 0;
+}
 
-	//declaracao de variaveis
-	int campos[8][8];
-	int jogador = 1;
-
-	iniciarJogo(campos);
-
-	jogadasValidas(campos):
-	imprimirJogo(campos);
-	limparJogadasValidas(campos);
-	jogada(jogador, campos);
-	imprimirJogo(campos);
-
-	return 0;
+void listarJogadas(int campos[8][8]){
+    printf("Jogadas válidas:\n");
+    for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 8; j++){
+            if (campos[i][j]==3){
+                printf("(%d,%d) ", i+1, j+1);
+            }
+        }
+    }
+    printf("\n");
 }
 
 
-//Mostra na tela o tabuleiro do jogo com X e O para as respectivas jogadas de cada jogador
+// Mostra o tabuleiro do jogo com X e O para as respectivas jogadas de cada jogador
 void imprimirJogo(int campos[8][8]) {
-	int i, j;
-	printf("---------------------------------\n");
-	
-	for (i = 0; i < 8; i++) {
-		printf("|");
-		for (j = 0; j < 8; j++) {
-			if (campos[i][j]>=0 && campos[i][j]<=2){
-				switch (campos[i][j]){
-					case 1:
-						printf(" x ");
-						break;
-					case 2:
-						printf(" o ");
-						break;
-					default:
-						printf("   ");
-						break;
-				}
-			}
-			else{
-				printf("error");
-				return;
-			}
-			printf("|");
-		}
-		printf("\n---------------------------------\n");
-	}
-	return;
+    printf("---------------------------------\n");
+    for (int i = 0; i < 8; i++) {
+        printf("|");
+        for (int j = 0; j < 8; j++) {
+            switch (campos[i][j]){
+                case 1:
+                    printf(" X "); break;
+                case 2:
+                    printf(" O "); break;
+                case 3:
+                    printf("( )"); break;
+                default:
+                    printf("   "); break;
+            }
+            printf("|");
+        }
+        printf("\n---------------------------------\n");
+    }
 }
 
-//Inicia o tabuleiro do jogo deixando somente com as pecas iniciais
+// Inicia o tabuleiro do jogo com as peças iniciais
 void iniciarJogo(int campos[8][8]){
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			campos[i][j] = 0;
+    for (int i = 0; i < 8; i++)
+        for (int j = 0; j < 8; j++)
+            campos[i][j] = 0;
+    
+    campos[3][4] = 1;
+    campos[4][3] = 1;
+    campos[3][3] = 2;
+    campos[4][4] = 2;
+
+}
+
+// Realiza a jogada no tabuleiro
+void jogada(int *jogador, int campos[8][8]) {
+    int linha, coluna;
+    while (1) {
+        printf("Digite linha e coluna para jogar: ");
+        scanf("%d %d", &linha, &coluna);
+
+        if (campos[linha - 1][coluna - 1] == 3) {
+            campos[linha - 1][coluna - 1] = *jogador;
+            comerPecas(*jogador, linha - 1, coluna - 1, campos);    // Chama a função para comer as peças
+            *jogador = (*jogador == 1) ? 2 : 1;                     // Troca de jogador após a jogada usando operador ternário
+            break;
+        } else {
+            printf("Jogada inválida, tente novamente.\n");
+        }
+    }
+    printf("\n");
+}
+
+
+// Verifica se a jogada é válida
+int validarJogada(int jogador, int linha, int coluna, int campos[8][8]) {
+    if (linha < 0 || linha >= 8 || coluna < 0 || coluna >= 8 || campos[linha][coluna] != 0)
+        return 1; // Jogada inválida
+
+    int direcoes[8][2] = {
+        {-1, -1}, {-1, 0}, {-1, 1},
+        {0, -1},           {0, 1},
+        {1, -1},  {1, 0},  {1, 1}
+    };
+    
+    for (int i = 0; i < 8; i++) {
+        int dx = direcoes[i][0];
+        int dy = direcoes[i][1];
+        int x = linha + dx;
+        int y = coluna + dy;
+
+        // Primeira verificação: deve haver uma peça adversária adjacente na direção
+        if (x >= 0 && x < 8 && y >= 0 && y < 8 && campos[x][y] == (jogador == 1 ? 2 : 1)) {
+            // Segunda verificação: deve haver uma peça do próprio jogador em linha
+            x += dx;
+            y += dy;
+            while (x >= 0 && x < 8 && y >= 0 && y < 8) {
+                if (campos[x][y] == jogador) {
+                    return 0; // Jogada válida
+                }
+                if (campos[x][y] == 0) {
+                    break; // Chegou a uma casa vazia antes de fechar o "sanduíche"
+                }
+                x += dx;
+                y += dy;
+            }
+        }
+    }
+    return 1; // Jogada inválida
+}
+
+// Calcula e marca jogadas válidas no tabuleiro
+int jogadasValidas(int jogador, int campos[8][8]) {
+    int validador = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (validarJogada(jogador, i, j, campos) == 0) {
+                campos[i][j] = 3; // Marca como jogada válida temporária
+                validador = 1;
+            }
+        }
+    }
+    return validador;
+}
+
+// Limpa as jogadas válidas temporárias do tabuleiro
+void limparJogadasValidas(int campos[8][8]) {
+    for (int i = 0; i < 8; i++)
+        for (int j = 0; j < 8; j++)
+            if (campos[i][j] == 3){
+                campos[i][j] = 0;
+	    }
+}
+
+// Loop principal do jogo
+void gameLoop() {
+    int campos[8][8], jogador = 1;
+    iniciarJogo(campos);
+
+    while (1) {
+        if (!jogadasValidas(jogador, campos)) {  //Uma vez que essa funcao eh usada como flag e como alteracao de campos
+						                         //temos que limpar as jogadas validas depois de usar ela
+
+		printf("Jogador %d não tem jogadas válidas.\n", jogador);
+		limparJogadasValidas(campos);
+		jogador = (jogador == 1) ? 2 : 1;
+
+		if (!jogadasValidas(jogador, campos)) {
+			limparJogadasValidas(campos); //talvez desnecessario, verificar
+			printf("Jogo acabou.\n");
+			break;
 		}
-	}
-	
-	campos[3][3]=1;
-        campos[4][4]=1;
-        campos[3][4]=2;
-        campos[4][3]=2;
+	    }
+    listarJogadas(campos);
+	limparJogadasValidas(campos);
+	jogadasValidas(jogador, campos);
+	imprimirJogo(campos);
+        jogada(&jogador, campos);
+        limparJogadasValidas(campos);
+    }
 
-
-	return;
+    fimDeJogo(campos);
 }
 
-//Preenche o vetor do jogo de forma que a jogada é gravada
-int jogada(int jogador, int campos[8][8]){
+// Determina o resultado final do jogo
+int fimDeJogo(int campos[8][8]) {
+    int pecasJogador1 = 0, pecasJogador2 = 0;
+    for (int i = 0; i < 8; i++)
+        for (int j = 0; j < 8; j++)
+            if (campos[i][j] == 1)
+                pecasJogador1++;
+            else if (campos[i][j] == 2)
+                pecasJogador2++;
 
-	int linha;
-	int coluna;
+    if (pecasJogador1 > pecasJogador2)
+        printf("Jogador 1 venceu!\n");
+    else if (pecasJogador2 > pecasJogador1)
+        printf("Jogador 2 venceu!\n");
+    else
+        printf("Empate.\n");
 
-	scanf("%d %d", &linha, &coluna);
-
-	if(jogador == 1){
-		campos[linha - 1][coluna - 1] = 1;
-		jogador = 2;
-		return 0;
-	}
-	if (jogador == 2){
-		campos[linha - 1][coluna - 1] = 2;
-		jogador = 1;
-		return 0;
-	}
-
-	return 1;
+    return 0;
 }
 
-int validarJogada(jogador, campos[8][8]){
-	
-	
+//Funcao para comer as pecas do adversario
+void comerPecas(int jogadorAtual, int posicaoLinha, int posicaoColuna, int tabuleiro[8][8]) {
+    int direcoes[8][2] = {
+        {-1, -1}, {-1, 0}, {-1, 1},
+        { 0, -1},          { 0, 1},
+        { 1, -1}, { 1, 0}, { 1, 1}
+    };
 
+    int adversario;
+    if (jogadorAtual == 1) {
+        adversario = 2;
+    } else {
+        adversario = 1;
+    }
 
-	return 1
+    for (int i = 0; i < 8; i++) {
+        int incrementoLinha = direcoes[i][0];
+        int incrementoColuna = direcoes[i][1];
+        int linhaAtual = posicaoLinha + incrementoLinha;
+        int colunaAtual = posicaoColuna + incrementoColuna;
+        int contadorPecas = 0;
+
+        while (linhaAtual >= 0 && linhaAtual < 8 && colunaAtual >= 0 && colunaAtual < 8) {
+            if (tabuleiro[linhaAtual][colunaAtual] == adversario) {
+                contadorPecas++;
+                linhaAtual += incrementoLinha;
+                colunaAtual += incrementoColuna;
+            } else if (tabuleiro[linhaAtual][colunaAtual] == jogadorAtual) {
+                while (contadorPecas > 0) {
+                    linhaAtual -= incrementoLinha;
+                    colunaAtual -= incrementoColuna;
+                    tabuleiro[linhaAtual][colunaAtual] = jogadorAtual;
+                    contadorPecas--;
+                }
+                break;
+            } else {
+                break;
+            }
+        }
+    }
 }
 
 
-//Calcula e retorna um vetor com todas as possibilidades de jogadas possíveis para o jogador atual
-void jogadasValidas(jogador, campos[8][8]){
-
-	for(int i = 0; i < 8; i++){
-		for(int j = 0; j < 8; j++){
-			if(validarJogada(jogador, campos[i][j] == 0){
-				campos[i][j] = 3;
-			}
-		}
-	}
-
-	return;
-}
-
-void limparJogadasValidas(campos[8][8]){
-	
-	for(int i = 0; i < 8; i++){
-		for(int j = 0; j < 8; j++){
-			if(campos[i][j] == 3){
-				campos[i][j] = 0;
-			}	
-		}
-	}
-
-	return;
-}
 
 
 //Funcoes para debug
 void debugJogo(int campos[8][8]){
+	printf("\n");
 	for (int i = 0; i < 8; i++){
 		for(int j = 0; j < 8; j++){
 			printf("%d", campos[i][j]);
 		}
 	}
+	printf("\n");
 }
